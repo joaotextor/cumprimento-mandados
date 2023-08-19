39fee7cc233680a9de316b41fe78cbf162c3f0bd
João Textor <joaotextor@gmail.com>
2023-08-19 14:57:28 -0300

    implement the functionality for querying cities and passing to parent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 "use client";
 import MyComboBox from "@/components/MyComboBox";
 import { cities } from "../cities.js";
+import { useEffect, useState } from "react";
+import { Tooltip } from "react-tooltip";
+import Image from "next/image.js";
+
+import questionIcon from "../images/question-icon.png";
 
 export default function Home() {
   const cityList: Array<string> = [""];
+  const [selectedCity, setSelectedCity] = useState<string>("");
+  const [mandado, setMandado] = useState<boolean>(false);
+  const [subsecao, setSubsecao] = useState<string>("");
+  const [UAA, setUAA] = useState<string>("");
 
   cities.forEach((city) => {
     city.cidadesAtendidas.forEach((cityName) => {
@@ -13,9 +22,66 @@ export default function Home() {
     });
   });
 
+  const handleInputChange = (value: string) => {
+    setSelectedCity(value);
+  };
+
+  useEffect(() => {
+    cities.forEach((city) => {
+      for (let i = 0; i < city.cidadesAtendidas.length; i++) {
+        if (city.cidadesAtendidas[i].name == selectedCity) {
+          setMandado(city.cidadesAtendidas[i].mandado);
+          setSubsecao(city.nomeCidade);
+          setUAA(city.cidadesAtendidas[i].UAA);
+        }
+      }
+    });
+  }, [selectedCity]);
+
   return (
-    <main className="h-screen flex p-2 items-center justify-center">
-      <MyComboBox itemList={cityList as string[]} />
+    <main className="h-screen flex flex-col p-2 items-center justify-center text-center">
+      <h1 className="text-gray-200">Cumprimento de Mandados</h1>
+      <div className="flex max-sm:flex-col max-sm:items-center">
+        <h3 className="sm:mb-4 max-sm:mb-1 text-gray-200">
+          Ferramenta para verificar se há cumprimento de mandados em cidades da
+          JFRS
+        </h3>
+        <div className="h-full ml-1 mb-1">
+          <a
+            href="https://www.trf4.jus.br/trf4/upload/editor/2023/apb17_pu_20230814153858_rrt15_consolida_correg.htm#x6"
+            target="_blank"
+          >
+            <Image
+              data-tooltip-id="my-tooltip"
+              src={questionIcon}
+              alt="question mark"
+              height={20}
+            />
+          </a>
+        </div>
+      </div>
+      <Tooltip
+        id="my-tooltip"
+        place="top"
+        variant="info"
+        content="De acordo com o Anexo VI do Provimento/TRF4 nº 62"
+      />
+      <MyComboBox itemList={cityList} handleInputChange={handleInputChange} />
+      {selectedCity != "" ? (
+        <div className="text-gray-200 mt-2 flex flex-col items-center">
+          <span className="font-bold">
+            {mandado
+              ? "Há cumprimento de Mandado"
+              : "Não há cumprimento de mandado"}
+          </span>
+          <span>
+            <span className="font-bold">Atendido por:</span>{" "}
+            {UAA ? UAA : `Subseção de ${subsecao}`}
+          </span>
+        </div>
+      ) : (
+        ""
+      )}
     </main>
   );
 }
--- src/components/MyComboBox/index.tsx
+++ src/components/MyComboBox/index.tsx
@@ -1,8 +1,14 @@
-import { Fragment, useState } from "react";
+"use client";
+import React, { Fragment, useState } from "react";
 import { Combobox, Transition } from "@headlessui/react";
 
+interface MyComboBoxProps {
+  itemList: string[];
+  handleInputChange: (value: string) => void;
+}
+
 const MyComboBox = (
-  { itemList }: { itemList: string[] },
+  { itemList, handleInputChange }: MyComboBoxProps,
   ...props: Array<React.HTMLAttributes<HTMLInputElement>>
 ) => {
   const [selectedCity, setSelectedCity] = useState(itemList[0]);
@@ -14,14 +20,23 @@ const MyComboBox = (
       : itemList.filter((city: string) => {
           return city.toLowerCase().includes(query.toLowerCase());
         });
+  const handleChange = (value: string) => {
+    setSelectedCity(value);
+    handleInputChange(value);
+  };
+
   return (
-    <Combobox value={selectedCity} onChange={setSelectedCity} {...props}>
+    <Combobox
+      value={selectedCity}
+      onChange={(event) => handleChange(event)}
+      {...props}
+    >
       <div className="relative mt-1">
         <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
           <Combobox.Input
             onChange={(event) => setQuery(event.target.value)}
             className="border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
-            placeholder="Selecione a cidade"
+            placeholder="Informe a cidade"
           />
         </div>
         <Transition
@@ -37,7 +52,7 @@ const MyComboBox = (
                 key={city}
                 className={({ active }) =>
                   `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                    active ? "bg-teal-600 text-white" : "text-gray-900"
+                    active ? "bg-eproc-secondary text-white" : "text-gray-900"
                   }`
                 }
                 value={city}
